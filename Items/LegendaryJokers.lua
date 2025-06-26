0df9058b917e6a2a26492316a15ef215631f7b52
sephdotwav <80807134+sephdotmp3@users.noreply.github.com>
2025-06-25 20:28:06 -0600

    minor formatting changes, added copyright

--- Items/LegendaryJokers.lua
+++ Items/LegendaryJokers.lua
@@ -1,4 +1,24 @@
-SMODS.Joker{
+--[[
+ * LegendaryJokers.lua
+ * This file is part of Mod of Theseus
+ *
+ * Copyright (C) 2025 Mod of Theseus
+ *
+ * Mod of Theseus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mod of Theseus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Skill Picker Reborn; if not, see <https://www.gnu.org/licenses/>.
+]]
+
+SMODS.Joker {
     key = "jinxJ",
     pos = {x = 0, y = 0},
     soul_pos = {x = 1, y = 0},
@@ -22,7 +42,7 @@ SMODS.Joker{
 
 }
 
-SMODS.Joker{
+SMODS.Joker {
     key = "altxxJ",
     pos = {x = 2, y = 0},
     soul_pos = {x = 3, y = 0},
@@ -44,7 +64,7 @@ SMODS.Joker{
 
 }
 
-SMODS.Joker{
+SMODS.Joker {
     key = "titanJ",
     pos = {x = 4, y = 0},
     soul_pos = {x = 5, y = 0},
