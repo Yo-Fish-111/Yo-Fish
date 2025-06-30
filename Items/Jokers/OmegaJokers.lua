b20c468ba64f2967ca76e92766ca17b8fb1ec0bb
Brian Moura <81476128+vrinee@users.noreply.github.com>
2025-06-30 19:15:57 +0000

    Added gacha joker and respective localization for en-us

--- Items/Jokers/OmegaJokers.lua
+++ Items/Jokers/OmegaJokers.lua
@@ -39,3 +39,147 @@ SMODS.Joker {
     end
     -- No relevant info for joker display
 }
+
+-- Helper function to roll rarity with weighted chances
+local function roll_rarity_weighted()
+    -- Define rarity chances (higher numbers = more common)
+    -- Rarity 1: 50%, Rarity 2: 25%, Rarity 3: 15%, Rarity 4: 7%, Rarity 5: 2.5%, Rarity 6: 0.5%
+    local rarity_weights = {
+        [1] = 500,  -- Common (50%)
+        [2] = 250,  -- Uncommon (25%)
+        [3] = 150,  -- Rare (15%)
+        [4] = 70,   -- Legendary (7%)
+        [5] = 25,   -- Superb (2.5%)
+        [6] = 5     -- Omega (0.5%)
+    }
+    
+    -- Calculate total weight
+    local total_weight = 0
+    for _, weight in pairs(rarity_weights) do
+        total_weight = total_weight + weight
+    end
+    
+    -- Roll random number using match seed
+    local roll = pseudorandom('gacha_rarity_' .. G.GAME.pseudorandom.seed) * total_weight
+    
+    -- Find which rarity was rolled
+    local current_weight = 0
+    for rarity = 1, 6 do
+        current_weight = current_weight + rarity_weights[rarity]
+        if roll <= current_weight then
+            return rarity
+        end
+    end
+    
+    return 1 -- Fallback to rarity 1
+end
+
+
+SMODS.Joker{
+    key = "gachaJokerJ",
+    pos = {x = 0, y = 0},
+    rarity = "mot_omega",
+    atlas = "PLH",
+    config = {extra = {cost = 10, rolls = 1,pity = 80, currentPity = 0,amountOfRolls = 0,rollIncrease = 5}},
+    cost = 15,
+    blueprint_compat = false,
+    loc_txt = {
+        name = "Gacha joker",
+        text = {
+            "Rolls a random joker every end of shop for {X:money,C:white}$#1#{}",
+            "Every {C:green}#6#{} rolls, increases how many rolls you get by {C:green}1{}",
+            "{C:inactive} #4#/#3# pity for legendary+",
+        }
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.cost, card.ability.extra.rolls, card.ability.extra.pity, card.ability.extra.currentPity, card.ability.extra.amountOfRolls, card.ability.extra.rollIncrease}}
+    end,
+
+    calculate = function(self, card, context)
+        if context.ending_shop and not context.blueprint then
+            if G.GAME.dollars < card.ability.extra.cost then -- check if player has enough money
+                return {
+                    message = localize('k_not_enough_money'),
+                    colour = G.C.MONEY
+                }
+            end
+            
+            -- Deduct money
+            ease_dollars(-card.ability.extra.cost)
+            
+            -- Perform the rolls
+            for i = 1, card.ability.extra.rolls do
+                local rolled_rarity = roll_rarity_weighted()
+                
+                -- Apply pity system for legendary+
+                if card.ability.extra.currentPity >= card.ability.extra.pity and rolled_rarity < 4 then
+                    rolled_rarity = math.max(4, rolled_rarity) -- Guarantee at least legendary
+                    card.ability.extra.currentPity = 0 -- Reset pity
+                elseif rolled_rarity >= 4 then
+                    card.ability.extra.currentPity = 0 -- Reset pity on natural high roll
+                end
+                local rarityTable = { --table for rarity names
+                    [1] = localize('k_common'),
+                    [2] = localize('k_uncommon'),
+                    [3] = localize('k_rare'),
+                    [4] = localize('k_legendary'),
+                    [5] = localize('k_mot_superb'),
+                    [6] = localize('k_mot_omega'),
+                    
+                }
+                local rarity_name = rarityTable[rolled_rarity]
+                local rarity_for_card = rolled_rarity
+                local legendary = nil
+
+                -- Determine rarity for card creation
+                if rolled_rarity == 1 then-- Common
+                    rarity_for_card = 0
+                elseif rolled_rarity == 2 then-- Uncommon
+                    rarity_for_card = 0.8
+                elseif rolled_rarity == 3 then-- Rare
+                    rarity_for_card = 1
+                elseif rolled_rarity == 4 then-- Legendary
+                    legendary = true
+                elseif rolled_rarity == 5 then-- Superb
+                    rarity_for_card = "mot_superb"
+                elseif rolled_rarity == 6 then-- Omega
+                    rarity_for_card = "mot_omega"
+                end
+                
+                -- Create joker using event system
+                G.E_MANAGER:add_event(Event({
+                    func = function() 
+                        local new_joker = create_card('Joker', G.jokers, legendary, rarity_for_card, nil, nil, nil, 'gacha')
+                        new_joker:add_to_deck()
+                        G.jokers:emplace(new_joker)
+                        new_joker:start_materialize()
+                        return true
+                    end
+                }))
+                
+                card_eval_status_text(card, 'extra', nil, nil, nil, {
+                    message = localize('k_rolled').." " .. rarity_name .. "!",
+                    colour = G.C.FILTER
+                })
+            end
+            
+            -- Update pity counter and rolls
+            card.ability.extra.amountOfRolls = card.ability.extra.amountOfRolls + card.ability.extra.rolls
+            card.ability.extra.currentPity = card.ability.extra.currentPity + card.ability.extra.rolls
+            
+            -- Check if we need to increase the number of rolls
+            if card.ability.extra.amountOfRolls >= card.ability.extra.rollIncrease then 
+                card.ability.extra.rolls = card.ability.extra.rolls + 1
+                card.ability.extra.amountOfRolls = 0
+            end 
+            
+            -- Message to player
+            return {
+                message = localize("k_mot_gacha"),
+                colour = G.C.MONEY
+            }
+        end
+    end
+    
+}
